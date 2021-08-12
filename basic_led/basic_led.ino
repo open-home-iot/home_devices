8ee6ebc9ba25d2100a48dacba9d1a9ef0cfd142e
Måns Thörnvik <thornvik.mans@gmail.com>
2021-08-12 14:40:10 +0200

    first device done

--- basic_led/basic_led.ino
+++ basic_led/basic_led.ino
@@ -4,15 +4,24 @@
 */
 
 // TODO: create HOME-specific header file.
-#define REQUEST_TYPE       0
-#define REQUEST_CAPABILITY 48 // ASCII 0
+#define REQUEST_TYPE        0
+#define REQUEST_STATE_GROUP 1
+#define REQUEST_NEW_STATE   2
+
+#define REQUEST_CAPABILITY  48 // ASCII 0
+#define REQUEST_ACTION      49 // ASCII 1
 
 #define MSG_START 94 // ASCII ^
 #define MSG_END   36 // ASCII $
 
-#define CAPABILITIES "^0{\"uuid\":\"0ededfd1-65aa-42ba-866e-fb5c5ad607f2\",\"category\":1,\"type\":1,\"states\":{\"control\":[\"on\",\"off\"]}}$"
+#define CAPABILITIES "^0{\"uuid\":\"0ededfd1-65aa-42ba-866e-fb5c5ad607f2\",\"name\":\"Basic LED\",\"category\":1,\"type\":1,\"states\":[{\"id\":0,\"control\":[{\"on\":1},{\"off\":0}]}]}$"
 
-uint8_t request[64];
+#define STATE_LED_OFF 48
+#define STATE_LED_ON  49
+
+#define LED_PIN 13
+
+uint8_t request[32];
 uint8_t cursor = 0;
 
 void setup() {
@@ -21,6 +30,8 @@ void setup() {
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Native USB only
   }
+
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
@@ -28,9 +39,6 @@ void loop() {
   {
     // Delay to await input
     delay(2);
-
-    uint8_t buffer[64];
-    int count = Serial.readBytes(buffer, sizeof(buffer));
 
     /*
        Iterate over read bytes and put together a full request body,
@@ -47,25 +55,21 @@ void loop() {
        - continue parsing input bytes
 
     */
+    uint8_t ch = Serial.read();
 
-    for (int i = 0; i < count; i++)
+    if (ch == MSG_START)
     {
-      uint8_t ch = buffer[i];
-
-      if (ch == MSG_START)
-      {
-        memset(request, 0, sizeof(request));
-        cursor = 0;
-      }
-      else if (ch == MSG_END)
-      {
-        execute_request();
-      }
-      else
-      {
-        request[cursor] = buffer[i];
-        cursor++;
-      }
+      memset(request, 0, sizeof(request));
+      cursor = 0;
+    }
+    else if (ch == MSG_END)
+    {
+      execute_request();
+    }
+    else
+    {
+      request[cursor] = ch;
+      cursor++;
     }
   }
 }
@@ -78,5 +82,18 @@ void execute_request() {
   if (request[REQUEST_TYPE] == REQUEST_CAPABILITY)
   {
     Serial.write(CAPABILITIES);
+  }
+  else if (request[REQUEST_TYPE] == REQUEST_ACTION)
+  {
+    // Ignore checking state group since only one exists.
+    
+    if (request[REQUEST_NEW_STATE] == STATE_LED_ON)
+    {
+      digitalWrite(LED_PIN, HIGH);
+    }
+    else if (request[REQUEST_NEW_STATE] == STATE_LED_OFF)
+    {
+      digitalWrite(LED_PIN, LOW);
+    }
   }
 }
